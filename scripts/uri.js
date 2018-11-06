365793b2eaca4f24a35d9d8377403472280f20c5
Viraj Bangari <13295484+Viraj3f@users.noreply.github.com>
2018-11-05 22:12:20 -0500

    Update uri.js

--- scripts/uri.js
+++ scripts/uri.js
@@ -2,7 +2,7 @@ function sample(ary) {
   return ary[Math.floor(Math.random() * ary.length)]
 }
 
-let coffeUri = sample([
+let coffeeUri = sample([
     "https://www.amazon.com/Aerobie-AeroPress-Coffee-Espresso-Filters/dp/B001HBCVX0/ref=sr_1_4/132-4841247-4719761?ie=UTF8&qid=1533104191&sr=8-4&keywords=aeropress+coffee+makers",
     "https://aeropressinc.com"
   ]);
